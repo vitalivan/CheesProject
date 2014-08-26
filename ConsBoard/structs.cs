ab28549649497a885b5a96e2805dbdf7a0576c2b
vitalivan <vitalivan@bk.ru>
2014-08-26 23:03:19 +0300

    добавлены свойства

--- ConsBoard/structs.cs
+++ ConsBoard/structs.cs
@@ -8,10 +8,40 @@ namespace ConsoleApplication1
     enum FigureColor { Black,White}
     static struct Move 
     {
+        public Move(int colFrom, int rowFrom, int colTo, int rowTo)
+        {
+            this.colFrom=colFrom;
+            this.rowFrom = rowFrom;
+            this.colTo = colTo;
+            this.rowTo = rowTo;
+        }
+
         int colFrom;
         int rowFrom;
         int colTo;
         int rowTo;
+
+        public int ColFrom
+        {
+            get { return colFrom; }
+            set { colFrom = value; }
+        }
+
+        public int RowFrom
+        {
+            get { return rowFrom; }
+            set { rowFrom = value; }
+        }
+        public int ColTo
+        {
+            get { return ColTo; }
+            set { ColTo = value; }
+        }
+        public int RowTo
+        {
+            get { return RowTo; }
+            set { RowTo = value; }
+        }
 
     }
 
